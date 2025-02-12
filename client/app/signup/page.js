e6d59a7a37ce05a302611f3c092eefba0a1c9d33
TitanProgrammer4480 <sebi-2007@web.de>
2025-02-12 18:24:08 +0100

    added form to the signup page

--- client/app/signup/page.js
+++ client/app/signup/page.js
@@ -1,10 +1,34 @@
-import Header from "@/components/Header";
+"use client"
 
 export default function SignUp() {
+
+    async function onLogin(event) {
+
+        const formData = new FormData(event.target)
+        const response = await fetch("", {
+          method: "POST",
+          body: formData
+        })
+    
+        const data = await response.json()
+        if(data) {
+          console.log(data)
+        } else {
+          console.log(data)
+        }
+      }
+
     return (
         <>
             <h1>SignUp</h1>
             <p>Tets</p>
+            <form onSubmit={onLogin}>
+                <label htmlFor="name">Your Name:</label><br/>
+                <input type="text" id="name" name="name" required/><br/>
+                <label htmlFor="password">Your Password:</label><br/>
+                <input type="password" id="password" name="password" required/><br/>
+                <button type="submit">Login</button>
+            </form>
         </>
     );
 }
