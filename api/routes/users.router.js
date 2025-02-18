21501f3e5fd1bc25c9d7a2870d12cf70e74bd10d
ZonaryTitan <106260496+TitanProgrammer4480@users.noreply.github.com>
2025-02-18 10:31:58 +0100

    Update users.router.js

--- api/routes/users.router.js
+++ api/routes/users.router.js
@@ -1,23 +1,23 @@
 import express from "express";
 var usersRouter = express.Router();
 
-router.get('/', function(req, res, next) {
+usersRouter.get('/', function(req, res, next) {
   res.send({"title":"get all users"});
 });
 
-router.get('/:id', function(req, res, next) {
+usersRouter.get('/:id', function(req, res, next) {
   res.send({"title":"get a spezific user"});
 });
 
-router.post('/', function(req, res, next) {
+usersRouter.post('/', function(req, res, next) {
   res.send({"title":"create a user"});
 });
 
-router.put('/:id', function(req, res, next) {
+usersRouter.put('/:id', function(req, res, next) {
   res.send({"title":"update a user"});
 });
 
-router.delete('/:id', function(req, res, next) {
+usersRouter.delete('/:id', function(req, res, next) {
   res.send({"title":"delete a user"});
 });
 
