1432f583b0fcce53fe22fa85b1f25ba5d0ce54f3
ZonaryTitan <106260496+TitanProgrammer4480@users.noreply.github.com>
2025-02-18 10:20:22 +0100

    Update and rename users.js to users.router.js

---
+++ api/routes/users.router.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var usersRouter = express.Router();
+
+router.get('/', function(req, res, next) {
+  res.send({"title":"get all users"});
+});
+
+router.get('/:id', function(req, res, next) {
+  res.send({"title":"get a spezific user"});
+});
+
+router.post('/', function(req, res, next) {
+  res.send({"title":"create a user"});
+});
+
+router.put('/:id', function(req, res, next) {
+  res.send({"title":"update a user"});
+});
+
+router.delete('/:id', function(req, res, next) {
+  res.send({"title":"delete a user"});
+});
+
+module.exports = usersRouter;
