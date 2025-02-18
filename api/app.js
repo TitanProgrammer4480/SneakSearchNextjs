76140419dfc23c72280d4ace77a906f6e2d4e3f4
ZonaryTitan <106260496+TitanProgrammer4480@users.noreply.github.com>
2025-02-18 10:21:34 +0100

    Update app.js

--- api/app.js
+++ api/app.js
@@ -1,6 +1,10 @@
 import express from "express";
 
+import usersRouter from "./routes/users.router";
+
 const app = express();
+
+app.use(usersRouter, "/api/v1/users");
 
 app.get("/", (req, res) => {
     res.send("Welcome");
