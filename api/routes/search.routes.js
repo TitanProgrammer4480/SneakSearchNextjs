6289e203d2898ea34d5956a7495b05beec9e5bbf
TitanProgrammer4480 <sebi-2007@web.de>
2025-02-19 16:13:25 +0100

    tried adding google search

--- api/routes/search.routes.js
+++ api/routes/search.routes.js
@@ -8,6 +8,8 @@ searchRouter.get('/', function(req, res, next) {
 
 searchRouter.get('/:id', function(req, res, next) {
     googleIt({'query': req.params.id}).then(results => {
+      console.log(req.params.id);
+      console.log(results);
       res.send({"results": results});
      }).catch(e => {
       res.send({"error": e});
